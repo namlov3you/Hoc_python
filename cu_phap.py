092981f83d7e411643f79bf1bb4638d5a35cc1fa
namlov3you <tientroi1998@gmail.com>
2025-12-24 19:51:44 +0700

    update thêm code

--- cu_phap.py
+++ cu_phap.py
@@ -1,2 +1,17 @@
 if 5>2:
     print("5 lon hon 2")
+
+x = 5
+y = "hello, world"
+
+print(x,y) # comment 1 dòng
+
+print("xin chao", end=" ") # in nhiều chữ trên 1  dòng
+print("Viet Nam")
+
+print("I am", 35, "years old.")
+"""
+Đây là comment 
+nhiều dòng
+"""
+print("OK")
