d8158ab5d6f5b7f5870afbdac1df0f1c23b072ed
namlov3you <tientroi1998@gmail.com>
2025-12-24 22:15:54 +0700

    Thêm code file bien.py

--- bien.py
+++ bien.py
@@ -14,3 +14,13 @@ print("Type of name:", type(name))
 print("Type of age:", type(age))
 print("Type of price:", type(price))
 print("Type of is_admin:", type(is_admin))
+
+print("---Phép toán với biến----")
+a = 15
+b = 4
+print("tổng = ", a + b)
+print("Hiệu = ", a - b)
+print("Tích = ", a * b)
+print("Thương = ",a / b )
+print("dư ", a, "chia ", b, " = ", a%b)
+print(a,"chia",b,"lấy phần nguyên = ", a//b)
